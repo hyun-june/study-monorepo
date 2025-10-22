13bcc2b026ec858c56b4c5e0f6cd7821b9c005bc
unknown <leehyunjune11@naver.com>
2025-10-22 21:43:47 +0900

    responsive

--- src/component/Box.js
+++ src/component/Box.js
@@ -1,13 +1,17 @@
-import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUser, faComputer } from '@fortawesome/free-solid-svg-icons'; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser, faComputer } from "@fortawesome/free-solid-svg-icons";
 
 const Box = (props) => {
-  const { item, result, title  } = props;
-  const defaultIcon = title  === "User" ? <FontAwesomeIcon icon={faCircleUser}/> : <FontAwesomeIcon icon={faComputer} />;
-    console.log("item",props)
+  const { item, result, title } = props;
+  const defaultIcon =
+    title === "User" ? (
+      <FontAwesomeIcon icon={faCircleUser} />
+    ) : (
+      <FontAwesomeIcon icon={faComputer} />
+    );
+
   return (
-    <div className={`box  ${title} ${result ? result : ''}`}>
+    <div className={`box  ${title} ${result ? result : ""}`}>
       <h1>{props.title}</h1>
       <div className="item-icon">{item ? item.icon : defaultIcon}</div>
       <h2>{props.result}</h2>
